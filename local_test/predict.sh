656ed21017d0c1a1d888f905dddfd7c5e95ec755
Zoran Pandovski <zoran.pandovski@keitaro.com>
2020-02-07 13:51:09 +0100

    Use data-binary to keep new lines

--- local_test/predict.sh
+++ local_test/predict.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 payload=$1
-content=${2:-application/json}
+content=$2
 
-curl -d @${payload} -H "Content-Type: ${content}" -v http://localhost:8080/invocations
+curl --data-binary  @${payload} -H "Content-Type: ${content}" -v http://localhost:8080/invocations
